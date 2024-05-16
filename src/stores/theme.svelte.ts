69c77ef8048f63f9e167e4b22393cc06ff6b39be
Noel Echo Fowlie <noel@fowlie.dev>
2024-05-16 01:05:40 -0700

    Theme is remembered across sessions

--- src/stores/theme.svelte.ts
+++ src/stores/theme.svelte.ts
@@ -1,13 +1,32 @@
 // export let theme = $state('dark')
 // export let isSolar = $state('')
 
+
 export function themeRender() {
-    const theme = $state({ light: true, solar: false })
+    let light = true, solar = false
+
+    $effect(() => {
+        light = localStorage.getItem('light') ? JSON.parse(localStorage.getItem('light')!) : true
+        solar = localStorage.getItem('solar') ? JSON.parse(localStorage.getItem('solar')!) : false
+        theme.light = light
+        theme.solar = solar
+    })
+
+    const theme = $state({ light: light, solar: solar })
 
     return {
         get light() { return theme.light },
         get solar() { return theme.solar },
-        toggleLight: () => theme.light = !theme.light,
-        toggleSolar: () => theme.solar = !theme.solar
+        toggleLight: () => {
+            theme.light = !theme.light;
+            localStorage.setItem('light', String(theme.light))
+            console.log(localStorage.getItem('light'))
+        },
+        toggleSolar: () => {
+            theme.solar = !theme.solar
+            localStorage.setItem('solar', String(theme.solar))
+
+        }
     }
+
 }
