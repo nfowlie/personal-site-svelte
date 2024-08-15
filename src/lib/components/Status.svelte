9177f3b75197d60b8fcf5f110dd0f37cd96109c5
Noel Echo Fowlie <noel@fowlie.dev>
2024-08-15 00:34:42 -0700

    Status Icons now function (missing lightbulb and sun)

--- src/lib/components/Status.svelte
+++ src/lib/components/Status.svelte
@@ -9,16 +9,32 @@
 	<button>
 		<LucideIcon name="Sun" />
 	</button>
-	<button>
+	<button
+		on:click={() => {
+			navigator.share({ url: document.URL });
+		}}
+	>
 		<LucideIcon name="Share" />
 	</button>
-	<button>
+	<button
+		on:click={() => {
+			window.open('tel:17046999544', '_self');
+		}}
+	>
 		<LucideIcon name="Phone" />
 	</button>
-	<button>
+	<button
+		on:click={() => {
+			window.open('mailto:noel@fowlie.dev', '_self');
+		}}
+	>
 		<LucideIcon name="Mail" />
 	</button>
-	<button>
+	<button
+		on:click={() => {
+			window.open('https://www.linkedin.com/in/noel-echo-fowlie/', 'blank');
+		}}
+	>
 		<LucideIcon name="Linkedin" />
 	</button>
 </div>
